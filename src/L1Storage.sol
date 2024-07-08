636f8e29ca6aaa07bab9bc11c3177fb99d566c78
Haichen Shen <haichen@scroll.io>
2024-07-07 23:12:31 -0700

    update

--- src/L1Storage.sol
+++ src/L1Storage.sol
@@ -6,23 +6,27 @@ pragma solidity ^0.8.20;
  * @title Storage
  * @dev Store & retrieve value in a variable
  */
-contract L1Storage {
+ contract L1Storage {
 
-    uint256 public number;
+    uint256 public num1;
+    uint256 public num2;
+    uint256 public num3;
 
     /**
      * @dev Store value in variable
-     * @param num value to store
+     * @param nums values to store
      */
-    function store(uint256 num) public {
-        number = num;
+    function store(uint256[3] calldata nums) public {
+        num1 = nums[0];
+        num2 = nums[1];
+        num3 = nums[2];
     }
 
     /**
      * @dev Return value
      * @return value of 'number'
      */
-    function retrieve() public view returns (uint256){
-        return number;
+    function retrieve() public view returns (uint256, uint256, uint256) {
+        return (num1, num2, num3);
     }
 }
--- src/L2Storage.sol
+++ src/L2Storage.sol
@@ -6,40 +6,29 @@ import {IL1Blocks} from "./IL1Blocks.sol";
 
 /**
  * @title L2Storage
- * @dev L2Storage retrieves value from L1Storage contract
+ * @dev L2Storage retrieves values from L1Storage contract
  */
-contract L2Storage {
-    address constant L1_BLOCKS_ADDRESS = 0x5300000000000000000000000000000000000001;
+ contract L2Storage {
     address constant L1_SLOAD_ADDRESS = 0x0000000000000000000000000000000000000101;
-    uint256 constant NUMBER_SLOT = 0;
+    uint256 constant NUM1_SLOT = 0;
+    uint256 constant NUM2_SLOT = 1;
+    uint256 constant NUM3_SLOT = 2;
     address immutable l1StorageAddr;
 
-    event L1BlockNumber(uint256 number);
-    event RetrievedNumber(uint256 number);
+    event RetrievedNumbers(uint256 num1, uint256 num2, uint256 num3);
 
     constructor(address _l1Storage) {
         l1StorageAddr = _l1Storage;
     }
 
-    function latestL1BlockNumber() public returns (uint256) {
-        uint256 l1BlockNum = IL1Blocks(L1_BLOCKS_ADDRESS).latestBlockNumber();
-        emit L1BlockNumber(l1BlockNum);
-        return l1BlockNum;
-    }
-
-    function retrieveFromL1() public returns (uint256) {
-        uint256 l1BlockNum = IL1Blocks(L1_BLOCKS_ADDRESS).latestBlockNumber();
-        bytes memory input = abi.encodePacked(l1BlockNum, l1StorageAddr, NUMBER_SLOT);
-        bool success;
-        bytes memory ret;
-        (success, ret) = L1_SLOAD_ADDRESS.call(input);
-        if (success) {
-            uint256 number;
-            (number) = abi.decode(ret, (uint256));
-            emit RetrievedNumber(number);
-            return number;
-        } else {
+    function loadFromL1() public returns (uint256, uint256, uint256) {
+        (bool success, bytes memory ret) = L1_SLOAD_ADDRESS.staticcall(
+            abi.encodePacked(l1StorageAddr, NUM1_SLOT, NUM2_SLOT, NUM3_SLOT)
+        );
+        if (!success) {
             revert("L1SLOAD failed");
         }
+        (uint256 num1, uint256 num2, uint256 num3) = abi.decode(ret, (uint256, uint256, uint256));
+        emit RetrievedNumbers(num1, num2, num3);
+        return (num1, num2, num3);
     }
-}
